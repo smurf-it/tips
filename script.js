1871e7b0ca8e8f734273ed64a05994945e76ee3e
smurf-it <132983528+smurf-it@users.noreply.github.com>
2023-09-26 14:45:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,87 @@
+
 var tips = [];
 
-        // Load tips from local storage on page load
-        window.addEventListener('load', function () {
-            var storedTips = localStorage.getItem('tips');
-            if (storedTips) {
-                tips = JSON.parse(storedTips);
-                updateTipList();
-            }
-        });
+// Load tips from cookies on page load
+window.addEventListener('load', function () {
+  var storedTips = getCookie('tips');
+  if (storedTips) {
+    tips = JSON.parse(storedTips);
+    updateTipList();
+  }
+});
 
-        function addTip() {
-            var tipInput = document.getElementById("tip");
-            var tipValue = parseFloat(tipInput.value);
+function addTip() {
+  var tipInput = document.getElementById("tip");
+  var tipValue = parseFloat(tipInput.value);
 
-            if (!isNaN(tipValue)) {
-                tips.push(tipValue);
-                tipInput.value = "";
-                updateTipList();
-                saveTipsToLocalStorage();
-            }
-        }
+  if (!isNaN(tipValue)) {
+    tips.push(tipValue);
+    tipInput.value = "";
+    updateTipList();
+    saveTipsToCookies();
+  }
+}
 
-        function removeTip(index) {
-            tips.splice(index, 1);
-            updateTipList();
-            saveTipsToLocalStorage();
-        }
+function removeTip(index) {
+  tips.splice(index, 1);
+  updateTipList();
+  saveTipsToCookies();
+}
 
-        function calculateSum() {
-            var sum = tips.reduce(function (a, b) {
-                return a + b;
-            }, 0);
+function calculateSum() {
+  var sum = tips.reduce(function (a, b) {
+    return a + b;
+  }, 0);
 
-            document.getElementById("sum").textContent = "Total Tips: €" + sum.toFixed(2);
-        }
+  document.getElementById("sum").textContent = "Total Tips: €" + sum.toFixed(2);
+}
 
-        function updateTipList() {
-            var tipList = document.getElementById("tipList");
-            tipList.innerHTML = "";
+function updateTipList() {
+  var tipList = document.getElementById("tipList");
+  tipList.innerHTML = "";
 
-            tips.forEach(function (tip, index) {
-                var li = document.createElement("li");
-                li.classList.add("tip-item");
+  tips.forEach(function (tip, index) {
+    var li = document.createElement("li");
+    li.classList.add("tip-item");
 
-                var span = document.createElement("span");
-                span.textContent = "€" + tip.toFixed(2) + " (Order " + (index + 1) + ")";
+    var span = document.createElement("span");
+    span.textContent = "€" + tip.toFixed(2) + " (Order " + (index + 1) + ")";
 
-                var button = document.createElement("button");
-                button.textContent = "X";
-                button.onclick = function () {
-                    removeTip(index);
-                };
+    var button = document.createElement("button");
+    button.textContent = "X";
+    button.onclick = function () {
+      removeTip(index);
+    };
 
-                li.appendChild(span);
-                li.appendChild(button);
-                tipList.appendChild(li);
-            });
-        }
+    li.appendChild(span);
+    li.appendChild(button);
+    tipList.appendChild(li);
+  });
+}
 
-        function saveTipsToLocalStorage() {
-            localStorage.setItem('tips', JSON.stringify(tips));
-        }
+function saveTipsToCookies() {
+  setCookie('tips', JSON.stringify(tips));
+}
 
-        function clearTips() {
-            tips = [];
-            updateTipList();
-            saveTipsToLocalStorage();
-        }
+function clearTips() {
+  tips = [];
+  updateTipList();
+  saveTipsToCookies();
+}
+
+// Helper function to get a cookie by name
+function getCookie(name) {
+  var cookies = document.cookie.split(';');
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+// Helper function to set a cookie
+function setCookie(name, value) {
+  document.cookie = name + '=' + value + '; path=/';
+}
