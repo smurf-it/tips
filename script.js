596ea0abc54f024de92e7590ba8430a5927d46d3
smurf-it <132983528+smurf-it@users.noreply.github.com>
2023-09-26 14:35:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
 var tips = [];
 
-function addTip() {
-    var tipInput = document.getElementById("tip");
-    var tipValue = parseFloat(tipInput.value);
+        // Load tips from local storage on page load
+        window.addEventListener('load', function () {
+            var storedTips = localStorage.getItem('tips');
+            if (storedTips) {
+                tips = JSON.parse(storedTips);
+                updateTipList();
+            }
+        });
 
-    if (!isNaN(tipValue)) {
-        tips.push(tipValue);
-        tipInput.value = "";
-        updateTipList();
-    }
-}
+        function addTip() {
+            var tipInput = document.getElementById("tip");
+            var tipValue = parseFloat(tipInput.value);
 
-function removeTip(index) {
-    tips.splice(index, 1);
-    updateTipList();
-}
+            if (!isNaN(tipValue)) {
+                tips.push(tipValue);
+                tipInput.value = "";
+                updateTipList();
+                saveTipsToLocalStorage();
+            }
+        }
 
-function calculateSum() {
-    var sum = tips.reduce(function (a, b) {
-        return a + b;
-    }, 0);
+        function removeTip(index) {
+            tips.splice(index, 1);
+            updateTipList();
+            saveTipsToLocalStorage();
+        }
 
-    document.getElementById("sum").textContent = "Total Tips: €" + sum.toFixed(2);
-}
+        function calculateSum() {
+            var sum = tips.reduce(function (a, b) {
+                return a + b;
+            }, 0);
 
-function updateTipList() {
-    var tipList = document.getElementById("tipList");
-    tipList.innerHTML = "";
+            document.getElementById("sum").textContent = "Total Tips: €" + sum.toFixed(2);
+        }
 
-    tips.forEach(function (tip, index) {
-        var li = document.createElement("li");
-        li.classList.add("tip-item");
+        function updateTipList() {
+            var tipList = document.getElementById("tipList");
+            tipList.innerHTML = "";
 
-        var span = document.createElement("span");
-        span.textContent = "€" + tip.toFixed(2) + " (Order " + (index + 1) + ")";
+            tips.forEach(function (tip, index) {
+                var li = document.createElement("li");
+                li.classList.add("tip-item");
 
-        var button = document.createElement("button");
-        button.textContent = "X";
-        button.onclick = function () {
-            removeTip(index);
-        };
+                var span = document.createElement("span");
+                span.textContent = "€" + tip.toFixed(2) + " (Order " + (index + 1) + ")";
 
-        li.appendChild(span);
-        li.appendChild(button);
-        tipList.appendChild(li);
-    });
-}
+                var button = document.createElement("button");
+                button.textContent = "X";
+                button.onclick = function () {
+                    removeTip(index);
+                };
+
+                li.appendChild(span);
+                li.appendChild(button);
+                tipList.appendChild(li);
+            });
+        }
+
+        function saveTipsToLocalStorage() {
+            localStorage.setItem('tips', JSON.stringify(tips));
+        }
+
+        function clearTips() {
+            tips = [];
+            updateTipList();
+            saveTipsToLocalStorage();
+        }
